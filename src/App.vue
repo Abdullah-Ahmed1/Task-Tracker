a058dfce2c402d3e5cc4550a6a22ef01432ccdf6
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2023-09-03 17:23:45 +0500

    button added

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script >
+import Header from './components/Header.vue'
+export default{
+  name:'App',
+  components:{
+    Header
+  }
+}
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
-  </main>
+   
+
+  </main> -->
+  <!-- <h1>hello world</h1> -->
+  <Header title ="hello"/>
 </template>
 
 <style scoped>
+
 header {
   line-height: 1.5;
 }
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <h1>Task Tracker {{title}}</h1>
+        <Button text = "Add Task" color = "green" />
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+export default{
+    name : 'Header',
+    props:['title'],
+    components:  {
+        Button
+    }
+}
+</script>
+
+<!-- scoped style is specfically for the this component -->
+<style scoped>
+    header{
+        display: block;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
